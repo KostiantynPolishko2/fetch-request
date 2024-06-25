6f0bcae7bdc16033dc8323511baa763c7392f427
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-25 07:37:35 +0300

    3 Inserted dictionary of type currencies.

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 import { CurrencyWrapper } from './Currency.styled';
+import { Dict } from 'styled-components/dist/types';
 
 interface CurrencyProps {}
 
@@ -26,9 +27,13 @@ interface IErrorData {
    response: IErrorStatus,
 }
 
-const Currency: FC<CurrencyProps> = () => {
+const client = axios.create({
+   baseURL: 'https://api.monobank.ua',
+})
 
-   const baseURL = 'https://api.monobank.ua/bank/currency';
+const nameCurrrency = new Map([[840, 'USD'], [980, 'UAH'], [978, 'EUR']]);
+
+const Currency: FC<CurrencyProps> = () => {
 
    let iCurrencyArray: ICurrencyArray = [{currencyCodeA: 0, currencyCodeB: 0, rateBuy: 0, rateSell: 0}];
    let iErrorData: IErrorData = {message: 'no msg', code: 'no code', name: 'no name', response: {status: 0}};
@@ -37,7 +42,7 @@ const Currency: FC<CurrencyProps> = () => {
    const [error, setError] = useState(iErrorData);
 
    React.useEffect(() => {
-     axios.get(baseURL).
+     client.get('/bank/currency').
      then((response) => { 
       setCurrency(response.data);
    }).
@@ -51,7 +56,11 @@ const Currency: FC<CurrencyProps> = () => {
 
    return(
       <CurrencyWrapper>
-         Currency is {currencies[0].currencyCodeA}: 
+         Currency rates for 1{nameCurrrency.get(840)}:
+         <ul>
+            <li>buy for&nbsp;<strong style={{color: 'green'}}>{currencies[0].rateBuy.toFixed(2)}</strong>{nameCurrrency.get(980)}</li>
+            <li>sell for&nbsp;<strong style={{color: 'red'}}>{currencies[0].rateSell.toFixed(2)}</strong>{nameCurrrency.get(980)}</li>
+         </ul>
       </CurrencyWrapper>
    );
 }
